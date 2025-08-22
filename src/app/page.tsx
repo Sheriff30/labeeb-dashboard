3637a117a03a5240868b4e800ea438d1da289597
sherif <sherifdevv@gmail.com>
2025-08-22 15:22:24 +0300

    added sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (pathname === "/") {
+      router.push("/login");
+    }
+  }, [pathname, router]);
+
   return <div></div>;
 }
---
+++ src/layout/RootSidebar/RootSidebar.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function RootSidebar() {
+  return <div className="bg-red-500 w-89 h-full">RootSidebar</div>;
+}
