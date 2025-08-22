31199f8d63717bbc9152084ee1f06b64c5541faa
sherif <sherifdevv@gmail.com>
2025-08-22 20:12:19 +0300

    layout updates

--- src/app/(dashboard)/school/layout.tsx
+++ src/app/(dashboard)/school/layout.tsx
@@ -7,9 +7,11 @@ type Props = {
 
 export default function layout({ children }: Props) {
   return (
-    <div className="grid grid-cols-[auto_1fr] h-screen">
+    <div className="grid grid-cols-[auto_1fr] h-screen bg-primary-2">
       <RootSidebar />
-      {children}
+      <div className="py-8 px-20 rounded-tr-[60px] rounded-br-[60px] bg-white">
+        {children}
+      </div>
     </div>
   );
 }
