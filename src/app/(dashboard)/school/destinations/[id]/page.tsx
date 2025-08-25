e3ec144fd6dd448cd515578e3df8ee8ee09a7484
sherif <sherifdevv@gmail.com>
2025-08-25 15:27:30 +0300

    using modals in school layout

--- src/app/(dashboard)/school/destinations/[id]/page.tsx
+++ src/app/(dashboard)/school/destinations/[id]/page.tsx
@@ -3,8 +3,6 @@ import { useParams } from "next/navigation";
 import React from "react";
 import { useDestination } from "@/hooks/useDestinations";
 import { Destination } from "@/views/Destination";
-import { useModal } from "@/Context";
-import { useRouter } from "next/navigation";
 
 export default function Page() {
   const params = useParams();
@@ -13,20 +11,6 @@ export default function Page() {
     : params?.id ?? "";
 
   const { data: destination, isLoading } = useDestination(id);
-  const router = useRouter();
-
-  const { openModal, closeModal } = useModal();
-
-  function handlemodal() {
-    openModal("SUCCESS", {
-      title: "تم تقديم الطلب بنجاح وسيتم تفعيل حسابكم في مدة أقصاها 24 ساعة",
-      buttonText: " شكراً",
-      onConfirm: () => {
-        router.push("/login");
-        closeModal();
-      },
-    });
-  }
 
   if (isLoading) {
     return <div className="text-2xl text-center">جاري تحميل الوجهة...</div>;
@@ -35,7 +19,7 @@ export default function Page() {
   return (
     <div>
       <Destination destination={destination} />
-      <button onClick={handlemodal}>lol</button>
+      <button></button>
     </div>
   );
 }
